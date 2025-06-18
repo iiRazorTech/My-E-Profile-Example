fddc437ddc0f43308973d33d6f9c57620608a6e9
Kevin <fcclea@gmail.com>
2025-06-18 16:20:55 -0400

    99% done

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
   if (contrastToggle) {
-    document.body.classList += " dark-theme"
+    document.body.classList += " dark-theme";
   }
   else {
-    document.body.classList.remove("dark-theme")
+    document.body.classList.remove("dark-theme");
   }
 }
 
@@ -39,8 +52,8 @@ function contact(event) {
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
-        return document.body.classList.remove("modal--open")
+        return document.body.classList.remove("modal--open");
     }
     isModalOpen = true;
-    document.body.classList += " modal--open"
+    document.body.classList += " modal--open";
 }
